a8ecebe3b507970c83b93e1550e24539c227e867
Alma <al.klein@posteo.de>
2021-02-12 18:23:23 +0100

    added number-check+ made some cleaning

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import ProgressBar from "./ProgressBar";
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <ProgressBar
-                    percentage={42}
-                    // maxValue={200}
-                    // minValue={0}
-                    // value={40}
-                />
-            </header>
+            <h1>ProgressBar component</h1>
+            <h2>Simple usage</h2>
+            <header className="App-header"></header>
+            <ProgressBar percentage={0} />
+            <ProgressBar percentage={1} />
+            <ProgressBar percentage={42} />
+            <ProgressBar percentage={99} />
+            <ProgressBar percentage={100} />
+            <h2>Value based usage</h2>
+            <ProgressBar value={20} minValue={0} maxValue={100} />
+            <h2>Error handling</h2>
+            <ProgressBar percentage={-4} />
+            <ProgressBar percentage={150} />
+            <ProgressBar maxValue={99} value={120} />
+            <ProgressBar minValue={10} value={-80} /> 
         </div>
     );
 }
--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,38 +1,36 @@
 import { useState, useEffect } from "react";
-import "./App.css";
-let progressWidth;
 
 export default function ProgressBar({ percentage, value, maxValue, minValue }) {
     const [error, setError] = useState(false);
     const [progress, setProgress] = useState(0);
 
     useEffect(() => {
-        if (percentage <= 100 && percentage >= 0) {
+        if (
+            typeof percentage === "number" &&
+            percentage <= 100 &&
+            percentage >= 0
+        ) {
             setProgress(percentage);
-        } else if (value >= 0 && value <= 200 && maxValue && minValue === 0) {
-            percentage = (value / maxValue) * 100;
-            setProgress(percentage);
-        } else if (percentage < 0) {
+        } else if (isNaN(value) || isNaN(maxValue) || isNaN(minValue)) {
             setError(true);
-            console.log("you provided a negative percentage");
-        } else if (percentage > 100) {
+            console.log("make sure the type of you values are numbers");
+        } else if (value >= minValue && value <= maxValue) {
+            setProgress((value / (maxValue - minValue)) * 100);
+        } else if (isNaN(percentage) || percentage < 0 || percentage > 100) {
             setError(true);
-            console.log("you provided a percentage higher than 100");
+            console.log(
+                "you provided an invalid percentage, it has to be typeof number higher 0 and lower 100"
+            );
         } else {
             setError(true);
             console.log("you provided an invalid value");
         }
     }, [percentage, value, maxValue, minValue]);
 
-    if (percentage === 0 || value === 0) {
-        progressWidth = "1%";
-    } else {
-        progressWidth = `${progress}%`;
-    }
+    const progressWidth = `${progress}%`;
 
     return (
         <>
-            <p>Progressbar</p>
             <div className="progress-bar">
                 {!error && (
                     <div
